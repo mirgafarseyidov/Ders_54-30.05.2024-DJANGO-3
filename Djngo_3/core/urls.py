244fe281799cc03a98761e1dae44d3463c4332fc
mirgafarseyidov <161619722+mirgafarseyidov@users.noreply.github.com>
2024-05-31 20:41:13 +0400

    First commit

---
+++ Djngo_3/core/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from article.views import *
+
+urlpatterns = [
+    path("", home__view, name="home"),
+    path("about/", about__view, name="about"),
+    path("contact/", contact__view, name="contact"),
+    path("articles/", articles__view, name="articles"),
+    path("article-detail/<int:id>", article__detail__view, name="article-detail"),
+    path("addarticle/", addarticle__view, name="addarticle"),
+    path("dashboard/", dashboard__view, name="dashboard"),
+    path("account/", include("account.urls")),
+    path('admin/', admin.site.urls),
+]
